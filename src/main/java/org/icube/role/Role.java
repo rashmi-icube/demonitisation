37ac4373fa85899b89c78ced553e53d53e53bc2a
swarna-icube <ssrivastava@i-cube.in>
2016-12-21 10:33:25 +0530

    added the object Role

---
+++ src/main/java/org/icube/role/Role.java
@@ -0,0 +1,33 @@
+package org.icube.role;
+
+public class Role {
+
+	private String role;
+	private int roleId;
+	private int candidateCount;
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public int getRoleId() {
+		return roleId;
+	}
+
+	public void setRoleId(int roleId) {
+		this.roleId = roleId;
+	}
+
+	public int getCandidateCount() {
+		return candidateCount;
+	}
+
+	public void setCandidateCount(int candidateCount) {
+		this.candidateCount = candidateCount;
+	}
+
+}
